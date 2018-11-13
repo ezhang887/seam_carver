fbb7173b4b8118433c572a97f77f66b649705323
Eric Zhang <ezhang887@gmail.com>
2018-11-12 18:26:36 -0600

    simplify

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -14,7 +14,7 @@ void ofApp::setup(){
 
     gui.setup();
     gui.add(button.setup("button"));
-    gui.add(input.setup("Set target width", 0, 0, 2000,200,30));
+    gui.add(input.setup("Set target width", 0));
 }
 
 void ofApp::buttonPressed(){
