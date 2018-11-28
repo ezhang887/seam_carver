bff20b31d4d3abc2c45812a04a798a1dcb425b0f
Eric Zhang <ezhang887@gmail.com>
2018-11-28 15:37:23 -0600

    continue work on tests

--- test/test.cpp
+++ test/test.cpp
@@ -19,15 +19,22 @@ class TestCarver : public SeamCarver{
 
 TEST_CASE("TEST"){
     vector<vector<Color>> image;
-    for(int i=0; i<3; i++){
+    /*
+     * (0,0,0), (0,1,1), (0,2,2), (0,3,3)
+     * (1,0,1), (1,1,2), (1,2,3), (1,3,4)
+     * (2,0,2), (2,1,3), (2,2,4), (2,3,5)
+     * (3,0,3), (3,1,4), (3,2,5), (3,3,6)
+     */
+    for(int i=0; i<4; i++){
         vector<Color> temp;
-        for(int j=0; j<3; j++){
+        for(int j=0; j<4; j++){
             temp.push_back(Color(i,j,i+j));
         }
         image.push_back(temp);
     }
     TestCarver t(image);
     REQUIRE(t.tp() == false);
-    REQUIRE(t.w() == 3);
-    REQUIRE(t.h() == 3);
+    REQUIRE(t.w() == 4);
+    REQUIRE(t.h() == 4);
+    vector<vector<double>> energy = {{1},{1},{1}};
 }
