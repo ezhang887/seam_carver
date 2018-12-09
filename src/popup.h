31efd67805680e03bec9164862a09b884285e555
Eric Zhang <ezhang887@gmail.com>
2018-12-08 19:30:37 -0600

    Revert "update popup for playing seams"
    This reverts commit 4747994b68b149b6fccd9b9d67e7e29c4fcaec59.
    
    revert

--- src/popup.h
+++ src/popup.h
@@ -1,27 +1,19 @@
 #ifndef POPUP_H
 #define POPUP_H
 
-#include <string>
-
 #include "ofMain.h"
 #include "ofxGui.h"
 #include "ofxCv.h"
-
-using std::string;
 
 class PopupWindow : public ofBaseApp{
 
     private:
         ofxButton save;
         ofImage image;
-        int update_duration;
-        bool should_update;
-        string path;
 
         void saveImage();
 
     public:
-        PopupWindow(string path, int update_duration);
         PopupWindow(ofImage image);
         void setup();
         void update();
@@ -29,7 +21,6 @@ class PopupWindow : public ofBaseApp{
 
 };
 
-void runPopupWindow(string path, int height, int width, shared_ptr<ofAppBaseWindow>& main_window);
 void runPopupWindow(ofImage image, shared_ptr<ofAppBaseWindow>& main_window);
 
 #endif
